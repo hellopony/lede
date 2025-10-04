7488f314468b770d2dddebe3da7e0ad99e91ed6e
hellopony <74644705+hellopony@users.noreply.github.com>
2025-10-04 20:01:28 +0800

    Update download.pl

--- scripts/download.pl
+++ scripts/download.pl
@@ -320,6 +320,7 @@ foreach my $mirror (@ARGV) {
 	}
 }
 
+push @mirrors, 'https://sources.immortalwrt.org/';
 push @mirrors, 'https://sources.cdn.openwrt.org';
 push @mirrors, 'https://sources.openwrt.org';
 push @mirrors, 'https://mirror2.openwrt.org/sources';
